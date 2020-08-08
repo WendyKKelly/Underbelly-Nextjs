7c8065e99e5109a1c851290da6a92339090e312e
Wendy Kelly <wendykelly@Wendys-iMac.local>
2020-08-08 15:10:14 -0700

    added data files

--- packages/common/src/data/AgencyDigital/index.js
+++ packages/common/src/data/AgencyDigital/index.js
@@ -10,6 +10,10 @@ import news1 from '../../assets/image/agencyDigital/news/1.png';
 import news2 from '../../assets/image/agencyDigital/news/2.png';
 import news3 from '../../assets/image/agencyDigital/news/3.png';
 import news4 from '../../assets/image/agencyDigital/news/4.png';
+import BlogImage1 from '../../assets/image/agencyDigital/blog/blog-img1.png';
+import BlogImage2 from '../../assets/image/agencyDigital/blog/blog-img2.png';
+import BlogImage3 from '../../assets/image/agencyDigital/blog/blog-img3.png';
+
 
 export const data = {
   navItems: [
@@ -19,81 +23,98 @@ export const data = {
       offset: '70',
     },
     {
-      label: 'Service',
+      label: '',
+      path: '#',
+      offset: '70',
+    },
+    {
+      label: '',
+      path: '#',
+      offset: '70',
+    },
+    {
+      label: 'Grow',
       path: '#service',
       offset: '70',
     },
     {
-      label: 'Pricing',
-      path: '#pricing',
-      offset: '70',
-    },
-    {
-      label: 'Our Customer',
-      path: '#our-customer',
-      offset: '70',
-    },
-    {
-      label: 'Blog',
+      label: 'Media',
       path: '#blog',
       offset: '70',
     },
   ],
-  services: [
+  blog: [
     {
       id: 1,
-      icon: serviceIcon1,
-      title: 'Email Subscription',
-      desc:
-        'We’re driven beyond just finishing projects. We want to find smart solutions.',
-      link: '#',
+      title: 'Train for Joy',
+      thumbnail_url: BlogImage1,
+      date: 'March 13, 2020',
+      postLink: '#1',
     },
     {
       id: 2,
-      icon: serviceIcon2,
-      title: 'Bolt Performance',
-      desc:
-        'We’re driven beyond just finishing projects. We want to find smart solutions.',
-      link: '#',
+      title: 'First Aid for Grief',
+      thumbnail_url: BlogImage2,
+      date: 'April 8, 2020',
+      postLink: '#2',
     },
     {
       id: 3,
-      icon: serviceIcon3,
-      title: 'Secure Transaction',
-      desc:
-        'We’re driven beyond just finishing projects. We want to find smart solutions.',
-      link: '#',
+      title: 'Provocation: How to Use it for Good',
+      thumbnail_url: BlogImage3,
+      date: 'January 4, 2020',
+      postLink: '#3',
+    },
+  ],
+  features: [
+    {
+      id: 1,
+      icon: 'flaticon-pencil-case',
+      title: 'Journaling',
+      description:
+        'There are so many ways to do this exercise. Old school, audio, video, find your best way to a daily journaling practice.',
+    },
+    {
+      id: 2,
+      icon: 'flaticon-creative',
+      title: 'Inquiry',
+      description:
+        'Take some time each day to check in with yourself.',
+    },
+    {
+      id: 3,
+      icon: 'flaticon-magnifying-glass',
+      title: 'Quests',
+      description:
+        'This is my preferred term for daily exercises to challenge yourself to continue your story: become a stronger, more flexible, more emotionally fit hero of your story.',
     },
     {
       id: 4,
-      icon: serviceIcon4,
-      title: 'Multiple Options',
-      desc:
-        'We’re driven beyond just finishing projects. We want to find smart solutions.',
-      link: '#',
+      icon: 'flaticon-startup',
+      title: 'Self Care',
+      description:
+        'This looks different for everyone. Take time, starting today, to build out your own self care kit, so when the time comes that you need a little self care first aid, you are ready.',
     },
     {
       id: 5,
-      icon: serviceIcon5,
-      title: 'Customer Support',
-      desc:
-        'We’re driven beyond just finishing projects. We want to find smart solutions.',
-      link: '#',
+      icon: 'flaticon-hourglass',
+      title: 'Meditation and Prayer',
+      description:
+        'There are many ways to approach meditation and prayer. Take time to find your own practice.',
     },
     {
       id: 6,
-      icon: serviceIcon6,
-      title: 'Integrated with Shopify',
-      desc:
-        'We’re driven beyond just finishing projects. We want to find smart solutions.',
-      link: '#',
+      icon: 'flaticon-group',
+      title: 'Therapy',
+      description:
+        'No one does this alone. Every hero needs a Guide. Find yours and begin your practice.',
     },
   ],
   workHardList: [
-    { id: 1, title: 'Medical and vision' },
-    { id: 2, title: 'Life insurance' },
-    { id: 3, title: '400(k) savings' },
-    { id: 4, title: 'HSAs and FSAs' },
+    { id: 1, title: 'OCEAN Big 5 ' },
+    { id: 2, title: 'Enneagram Assessment' },
+    { id: 3, title: 'Jungian Archetypes' },
+    { id: 4, title: '16 Personalities' },
   ],
   pricing: [
     {
@@ -233,52 +254,118 @@ export const data = {
       ],
     },
   ],
+  aboutus: [
+    {
+      id: 1,
+      title: 'Amazing communication experience.',
+      icon: 'flaticon-next',
+    },
+    {
+      id: 2,
+      title: 'Best designing experience with trending tools and sizes.',
+      icon: 'flaticon-next',
+    },
+    {
+      id: 3,
+      title: 'Training and communication method remotely.',
+      icon: 'flaticon-next',
+    },
+    {
+      id: 4,
+      title: '24/7 Hour onine supports.',
+      icon: 'flaticon-next',
+    },
+  ],
+  services: [
+    {
+      id: 1,
+      icon: serviceIcon1,
+      title: '10 Wild and precious things',
+      desc:
+        'A free weekly newsletter every Monday morning to start your week off right.',
+      link: '#',
+    },
+    {
+      id: 2,
+      icon: serviceIcon2,
+      title: 'Stories from my underbelly',
+      desc:
+        'An every so often blog hosted on Medium.',
+      link: '#',
+    },
+    {
+      id: 3,
+      icon: serviceIcon3,
+      title: 'Wild and precious conversations',
+      desc:
+        'A podcast every Thursday with guests who speak freely with me on topics related to your emotional health.',
+      link: 'https://wildprecious.thinkific.com/',
+    },
+    {
+      id: 4,
+      icon: serviceIcon4,
+      title: 'Journal',
+      desc:
+        'A 365 day journal to take you through a year of emotional growth. Pay-as-you-will',
+      link: 'https://wildprecious.thinkific.com/',
+    },
+    {
+      id: 5,
+      icon: serviceIcon5,
+      title: 'Life Rules',
+      desc:
+        'A guided template to take you through the process of writing your own life rules. Pay-as-you-will',
+      link: 'https://wildprecious.thinkific.com/',
+    },
+    {
+      id: 6,
+      icon: serviceIcon6,
+      title: 'Discover Yourself',
+      desc:
+        'Take a course to discover and gain compassion for your inner critic.',
+      link: 'https://wildprecious.thinkific.com/',
+    },
+
+  ],
   helps: [
     {
       id: 1,
       icon: support1,
-      title: 'Email client support',
+      title: 'Psychology Today <Find a Therapist>',
       desc:
-        'Ultimate access to all credit popular exercises and assessments materials',
+        'A great list of therapists you can serach for by location and other preferences.',
     },
-    {
-      id: 2,
-      icon: support2,
-      title: 'Live ticket support',
-      desc:
-        'Total assessment corrections with live support tickets download access system',
-    },
+
   ],
   newsFeed: [
     {
       id: 1,
       image: news1,
-      title: 'Introducing our newest team with great experience',
+      title: '',
       desc:
-        'Brian Halligan knows that you need more than a great product to have a great brand.',
-      link: '#',
+        'Curious about your emotional wellness? Assessments are a great way to learn more about yourself, what makes you tick, and how you can best take good care of your beautiful self.',
+      link: '#check-in',
     },
     {
       id: 2,
       image: news2,
-      title:
-        'New banking application has  developed and we expecting good feedback',
-      desc: '',
-      link: '#',
+      title: '',
+      desc: 'You can (and should) train your emotional self as much as you train your physcial body. If you take steps each day to get emotionally stronger and more flexible, when storms come, you will be ready',
+      link: '#train',
     },
     {
       id: 3,
       image: news3,
-      title: 'Ui/UX industry are doing great job in previous year history',
-      desc: '',
-      link: '',
+      title: '',
+      desc: 'Are you feeling pretty good but want to learn more about how your emotional self ticks? Start here to read, listen, and learn about how to care for your wild and precious self.',
+      link: '#learn',
     },
     {
       id: 4,
       image: news4,
-      title: 'Develop you design experience with figma features.',
-      desc: '',
-      link: '',
+      title: '',
+      desc: 'Everybody hurts sometimes. It happens to all of us. If you are suffering now, take needed next steps to care for yourself — Now.',
+      link: '#sad',
     },
   ],
   footer: [
@@ -437,26 +524,26 @@ export const data = {
     {
       id: 1,
       title: 'Home',
-      link: '#',
+      link: '/',
     },
     {
       id: 2,
-      title: 'Advertise',
-      link: '#',
+      title: 'About',
+      link: '#about',
     },
     {
       id: 3,
-      title: 'Supports',
+      title: '',
       link: '#',
     },
     {
       id: 4,
-      title: 'Marketing',
-      link: '#',
+      title: 'Support Our Mission',
+      link: 'https://www.patreon.com/underbellyproject',
     },
     {
       id: 5,
-      title: 'FAQ',
+      title: '',
       link: '#',
     },
   ],
